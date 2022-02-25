916d00d3ed4f326ec664c570f308eb754b76af00
dvalkova <dvalkova@vmware.com>
2022-02-25 18:16:54 +0200

    Update to cloud DB

---
+++ amld-correlation-example-job/01_create_covid_cases_usa_daily.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS covid_cases_usa_daily (
+    obs_date DATE,
+    number_of_cases INTEGER
+)
---
+++ amld-correlation-example-job/03_create_yankee_candle_reviews_transformed.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS yankee_candle_reviews_transformed (
+    date DATE,
+    num_negative_reviews INTEGER,
+    num_no_scent_reviews INTEGER
+)
